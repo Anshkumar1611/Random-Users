b361f9aa6faa74baa4951baed1a0a9c97662c991
Ansh kumar <86037839+Anshkumar1611@users.noreply.github.com>
2022-08-05 16:42:49 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-// import Card from './Card'
+import Card from './Card'
 
 function Hero({result,dataFetching}) {
     return (
         <div className="hero min-h-screen">
             <div className="hero-content text-center">
                 <div className="max-w-md">
-                   {/* <Card result={result} dataFetching={dataFetching}/> */}
+                  <Card result={result} dataFetching={dataFetching}/> 
                 </div>
             </div>
         </div>
